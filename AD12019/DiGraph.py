be08a346af64743c8a0de0f713d224d9e40677fa
william <williamguilhermesouza@gmail.com>
2019-12-28 15:27:58 -0300

    created structure for ad1 2019

---
+++ AD12019/DiGraph.py
@@ -0,0 +1,10 @@
+# classe de arestas
+class Edge:
+    pass
+
+#classe do digrafo
+class DiGraph:
+    pass
+
+if __name__ == '__main__':
+    pass
---
+++ AD12019/SocialGraph.py
@@ -0,0 +1,6 @@
+# cria um grafo de amigos para uma rede social
+class SocialGraph:
+    pass
+
+if __name__ == '__main__':
+    pass

f6a97b0dc7cc3b760d34faed6795b55b277273ba
william <williamguilhermesouza@gmail.com>
2019-12-30 17:14:35 -0300

    added infile,outfile and socialgraph methods

--- AD12019/SocialGraph.py
+++ AD12019/SocialGraph.py
@@ -1,6 +1,22 @@
 # cria um grafo de amigos para uma rede social
 class SocialGraph:
-    pass
+    def __init__(self):
+        pass
+
+    def add(self):
+        pass
+
+    def remove(self):
+        pass
+
+    def showFriends(self):
+        pass
+
+    def shortestPath(self):
+        pass
+
+    def recommendFriends(self dist=None, weightedDist=None):
+        pass
 
 if __name__ == '__main__':
     pass

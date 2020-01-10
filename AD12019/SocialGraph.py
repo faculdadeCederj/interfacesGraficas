aa14218684db53d65ee8aba9d7fdf2cdc9b14262
william <williamguilhermesouza@gmail.com>
2020-01-10 17:36:26 -0300

    beginning to work on SocialGraph, init issues

--- AD12019/SocialGraph.py
+++ AD12019/SocialGraph.py
@@ -1,9 +1,24 @@
 import DiGraph
+import sys
+commands = sys.argv[1]
 
 # cria um grafo de amigos para uma rede social
 class SocialGraph(DiGraph.DiGraph):
-    def __init__(self):
-        pass
+    def __init__(self, inputfile):
+        super().__init__(self)
+        with open(inputfile, 'r') as input:
+            for line in input:
+                allcommand = line.split()
+                command = allcommand[0]
+                allcommand.pop(0)
+                arguments = ''
+                for argument in allcommand:
+                    arguments += argument + ','
+                arguments = arguments[:-1]
+                with open('outfile.txt', 'a') as output:
+                    output.write(self.command(arguments))
+
+
 
     def add(self):
         pass
@@ -21,4 +36,4 @@ class SocialGraph(DiGraph.DiGraph):
         pass
 
 if __name__ == '__main__':
-    pass
+    graph = SocialGraph(commands)
